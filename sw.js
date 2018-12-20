97ded88f52cf543b0ab3911b7a0dd400880c5829
ejbulaong <SD38@mitt.ca>
2018-12-20 11:09:38 -0600

    update sw.js

--- sw.js
+++ sw.js
@@ -19,16 +19,15 @@ self.addEventListener("install", event=> {
         caches.open(staticCache).then((cache)=> {
             return cache.addAll(
                 [
-                    "/images/image0.jpg",
-                    "/images/image1.jpg",
-                    "/images/image2.jpg",
-                    "/images/image3.jpg",
-                    "/images/image4.jpg",
-                    "/styles/styles.css",
+                    "images/image0.jpg",
+                    "images/image1.jpg",
+                    "images/image2.jpg",
+                    "images/image3.jpg",
+                    "images/image4.jpg",
+                    "styles/styles.css",
                     "/",
-                    "/index.html",
-                    "app.js",
-                    "sw.js"
+                    "index.html",
+                    "app.js"
                 ]
             );
         })
